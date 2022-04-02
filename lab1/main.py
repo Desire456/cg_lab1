4d10f8ddebc40d0e18db0de85cce78ee26af5824
Vladislav Selitskii <selickiy.vladislav@gmail.com>
2022-04-02 22:22:50 +0400

    lab2

--- lab1/main.py
+++ lab1/main.py
@@ -1,18 +1,53 @@
 import math
+import random
+from typing import List
+
 from PIL import Image, ImageDraw
 import numpy as np
+from lab2.main import *
+
+
+class Point:
+    def __init__(self, x, y, z):
+        self.__x = x
+        self.__y = y
+        self.__z = z
+
+    @property
+    def x(self):
+        return self.__x
+
+    @property
+    def y(self):
+        return self.__y
+
+    @property
+    def z(self):
+        return self.__z
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
+
+    @z.setter
+    def z(self, value):
+        self.__z = value
 
 
 def part1(rows, col):
     image1 = np.zeros((rows, col), dtype=np.uint8)
     imageBlack = Image.fromarray(image1)
-    imageBlack.save('myBlack.png')
+    imageBlack.save('out/myBlack.png')
     image2 = np.full((rows, col), 255, dtype=np.uint8)
     imageWhite = Image.fromarray(image2)
-    imageWhite.save('myWhite.png')
+    imageWhite.save('out/myWhite.png')
     image3 = np.full((rows, col, 3), (255, 0, 0), dtype=np.uint8)
     imageRed = Image.fromarray(image3)
-    imageRed.save('myRed.png')
+    imageRed.save('out/myRed.png')
     image4 = np.zeros((rows, col, 3), dtype=np.uint8)
     # np.full((rows, col, 3), [100, 50, 200], dtype=np.uint8)
     for x in range(0, rows):
@@ -20,7 +55,7 @@ def part1(rows, col):
             v = round((x + y) % 256)
             image4[x][y] = (v, v, v)
     imageReg = Image.fromarray(image4)
-    imageReg.save('myReg.png')
+    imageReg.save('out/myReg.png')
 
 
 class Color3:
@@ -45,6 +80,7 @@ class Image3:
         self.h = h
         self.data = np.zeros(w * h, dtype=Color3)
         self.initial_data()
+        self.z_matrix = np.zeros((w, h))
 
     def set(self, x, y, color3):
         self.data[x + y * self.w] = color3
@@ -66,6 +102,48 @@ class Image3:
                     image_arr[i, j] = [color3.r, color3.g, color3.b]
         Image.fromarray(image_arr).save(filename)
 
+    def draw_triangle(self, points: List[Point]):
+        if len(points) != 3:
+            raise Exception('Error', 'Incorrect count of points')
+
+        x_list = list(map(lambda e: e.x, points))
+        y_list = list(map(lambda e: e.y, points))
+        y_min = min(y_list)
+        x_min = min(x_list)
+        y_max = max(y_list)
+        x_max = max(x_list)
+        if x_max > self.h or x_min < 0 or y_max > self.w or y_min < 0:
+            raise Exception('Error', 'Triangle coordinates are out of bounds')
+
+        point1 = points[0]
+        point2 = points[1]
+        point3 = points[2]
+        n = np.cross([point2.x - point1.x, point2.y - point1.y, point2.z - point1.z],
+                     [point2.x - point3.x, point2.y - point3.y, point2.z - point3.z])
+        v = [0, 0, 1]
+        cos_alpha = (n @ v) / np.sqrt(n[0] ** 2 + n[1] ** 2 + n[2] ** 2)
+        if cos_alpha > 0:
+            return
+        color = Color3(255 * abs(cos_alpha), 0, 0)
+        try:
+            for x in range(round(x_min), round(x_max)):
+                for y in range(round(y_min), round(y_max)):
+                    bc = barycentric_coordinates(x, y, point1.x, point1.y, point2.x, point2.y, point3.x, point3.y)
+                    if np.all(bc >= 0):
+                        z_val = bc[0] * point1.z + bc[1] * point2.z + bc[2] * point3.z
+                        if z_val > self.z_matrix[x][y]:
+                            self.z_matrix[x][y] = z_val
+                            self.set(x, y, color)
+        except Exception as e:
+            print(e.args)
+
+    def draw_polygons(self, points: List[Point], polygons: List):
+        for i in range(len(polygons)):
+            polygon1 = polygons[i][0]
+            polygon2 = polygons[i][1]
+            polygon3 = polygons[i][2]
+            self.draw_triangle([points[polygon1 - 1], points[polygon2 - 1], points[polygon3 - 1]])
+
 
 def part3_1(x0, y0, image, color):
     for i in range(0, 13):
@@ -76,8 +154,7 @@ def part3_1(x0, y0, image, color):
             x = round(x0 * (1 - t) + x1 * t)
             y = round(y0 * (1 - t) + y1 * t)
             image.putpixel((x, y), color)
-        image.save('var1.png')
-    image.save('var1.png')
+    image.save('out/var1.png')
 
 
 def part3_2(x0, y0, image, color):
@@ -89,7 +166,7 @@ def part3_2(x0, y0, image, color):
             t = (x - x0) / (x1 - x0)
             y = round(y0 * (1 - t) + y1 * t)
             image.putpixel((round(x), y), color)
-    image.save('var2.png')
+    image.save('out/var2.png')
 
 
 def part3_3(x0, y0, image, color):
@@ -114,7 +191,7 @@ def part3_3(x0, y0, image, color):
                 image.putpixel((round(y), round(x)), color)
             else:
                 image.putpixel((round(x), round(y)), color)
-    image.save('var3.png')
+    image.save('out/var3.png')
 
 
 def part3_4(x0, y0, x1, y1, image, color):
@@ -151,7 +228,7 @@ def part3_4draw(image, color):
     for i in range(0, 13):
         part3_4(100, 100, 100 + 95 * math.cos(2 * math.pi * i / 13), 100 + 95 * math.sin(2 * math.pi * i / 13), image,
                 color)
-        image.save('var4.png')
+        image.save('out/var4.png')
 
 
 class OBJ3DModel:
@@ -176,7 +253,7 @@ class OBJ3DModel:
                 polygon_infos = line.split()[1:]
                 polygon = []
                 for polygon_info in polygon_infos:
-                    polygon.append(int(polygon_info.split('/')[0]) - 1)
+                    polygon.append(int(polygon_info.split('/')[0]))
                 polygons.append(polygon[:3])
 
         verticles = list(map(lambda x: list(map(lambda b: float(b), x)), verticles))
@@ -202,7 +279,7 @@ def part5(model: OBJ3DModel, scale: int = 10, shift: int = 500):
         y = round(-scale * point[1] + shift)
         if 0 <= x < 1000 and 0 <= y < 1000:
             imageBlack.putpixel((x, y), 255)
-    imageBlack.save('vertex-st.png')
+    imageBlack.save('out/vertex-st.png')
 
 
 def part7(model: OBJ3DModel, scale: int = 50, shift: int = 500):
@@ -215,7 +292,7 @@ def part7(model: OBJ3DModel, scale: int = 50, shift: int = 500):
                 scale * polygon_point[2][0] + shift, -scale * polygon_point[2][1] + shift, imageBlack, 255)
         part3_4(scale * polygon_point[0][0] + shift, -scale * polygon_point[0][1] + shift,
                 scale * polygon_point[2][0] + shift, -scale * polygon_point[2][1] + shift, imageBlack, 255)
-    imageBlack.save('poligon-st.png')
+    imageBlack.save('out/poligon-st.png')
 
 
 if __name__ == '__main__':
@@ -232,7 +309,7 @@ if __name__ == '__main__':
     imageBlack = Image.fromarray(image1)
     part3_4draw(imageBlack, color=255)
     # -------------5-----------
-    obj3dmodel = OBJ3DModel('StormTrooper.obj')
+    obj3dmodel = OBJ3DModel('stuff/StormTrooper.obj')
     part5(obj3dmodel, scale=100)
     # --------------7-----------
     part7(obj3dmodel, scale=100)
---
+++ lab2/main.py
@@ -0,0 +1,29 @@
+from lab1.main import *
+
+
+# 1
+
+
+def barycentric_coordinates(x, y, x0, y0, x1, y1, x2, y2):
+    lambda0 = ((x1 - x2) * (y - y2) - (y1 - y2) * (x - x2)) / ((x1 - x2) * (y0 - y2) - (y1 - y2) * (x0 - x2))
+    lambda1 = ((x2 - x0) * (y - y0) - (y2 - y0) * (x - x0)) / ((x2 - x0) * (y1 - y0) - (y2 - y0) * (x1 - x0))
+    lambda2 = ((x0 - x1) * (y - y1) - (y0 - y1) * (x - x1)) / ((x0 - x1) * (y2 - y1) - (y0 - y1) * (x2 - x1))
+    return np.array([lambda0, lambda1, lambda2])
+
+
+if __name__ == '__main__':
+    image3 = Image3(1000, 1000)
+
+    points = [Point(100, 100, 100), Point(400, 300, 125), Point(900, 250, 0)]
+    # image3.draw_triangle(points, Color3(0, 0, 0))
+    # image3.save('../out/triangle.png')
+
+    obj3dmodel = OBJ3DModel('../stuff/fox.obj')
+    points = list(map(lambda point: Point(round(900 - 10 * point[1]),
+                                          round(10 * point[0] + 500),
+                                          round(10 * point[2])),
+                      obj3dmodel.get_verticles()))
+    polygons = obj3dmodel.get_polygons()
+    image3.draw_polygons(points, polygons)
+    image3.save('../out/fox.png')
+    # image3.draw_polygons()
