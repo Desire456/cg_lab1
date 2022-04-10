8150cb942acb4bd0436f51faf54d22cebeb62483
NastyAnanasty <angrybirds280700@gmail.com>
2022-04-10 16:00:47 +0400

    4 лаба

--- lab2/main.py
+++ lab2/main.py
@@ -5,9 +5,12 @@ from lab1.main import *
 
 
 def barycentric_coordinates(x, y, x0, y0, x1, y1, x2, y2):
-    lambda0 = ((x1 - x2) * (y - y2) - (y1 - y2) * (x - x2)) / ((x1 - x2) * (y0 - y2) - (y1 - y2) * (x0 - x2))
-    lambda1 = ((x2 - x0) * (y - y0) - (y2 - y0) * (x - x0)) / ((x2 - x0) * (y1 - y0) - (y2 - y0) * (x1 - x0))
-    lambda2 = ((x0 - x1) * (y - y1) - (y0 - y1) * (x - x1)) / ((x0 - x1) * (y2 - y1) - (y0 - y1) * (x2 - x1))
+    try:
+        lambda0 = ((x1 - x2) * (y - y2) - (y1 - y2) * (x - x2)) / ((x1 - x2) * (y0 - y2) - (y1 - y2) * (x0 - x2))
+        lambda1 = ((x2 - x0) * (y - y0) - (y2 - y0) * (x - x0)) / ((x2 - x0) * (y1 - y0) - (y2 - y0) * (x1 - x0))
+        lambda2 = ((x0 - x1) * (y - y1) - (y0 - y1) * (x - x1)) / ((x0 - x1) * (y2 - y1) - (y0 - y1) * (x2 - x1))
+    except Exception as e:
+        print(x, y, x0, y0, x1, y1, x2, y2)
     return np.array([lambda0, lambda1, lambda2])
 
 
